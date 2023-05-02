0345119229ce834f8504b5ec5dd106ce98c768be
Ross Keenan <ross.keenan@superbshifts.com>
2023-05-02 16:06:24 -0500

    Finished signup endpoint and method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,19 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { message: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    delete user.hash;
+
+    return user;
   }
 }
